4de0c2eb62a3f448a3c9508980925c6946438f5b
jimin <86237475+jinnins@users.noreply.github.com>
2023-04-04 22:41:48 +0900

    Update strings.dart

--- lib/resource/strings.dart
+++ lib/resource/strings.dart
@@ -3,25 +3,25 @@ class Strings {
   static final welcomeTxt = '''안녕하세요.''';
   static final name = '''제 이름은 송지민입니다.'''; // add . at the end
   static final whatIdo =
-  '''Windows 프로그램, 웹/앱을 개발하고 있습니다.'''; // add . at the end
+  '''Winform을 통한 Windows 프로그램을 개발하고 있습니다.'''; // add . at the end
   static final introAbout =
-  '''현재 제조업체에서 3년째 CAD 프로그램을 개발하고 있으며, 크로스플랫폼 개발에 관심이 많습니다.''';
+  '''현재 제조업체에서 4년째 CAD 프로그램을 개발하고 있으며, 크로스플랫폼 개발에 관심이 많습니다.''';
   static final currentOrgName = ''''''; // add . at the end
 
   /* About screen contents */
   static final aboutPara1 =
   '''저는 광운대학교 로봇학부를 졸업했습니다. 졸업작품으로 만든 축구로봇 알고리즘을 시작으로 프로그래밍에 관심이 생겼으며 2020년 3월부터 C# 개발을 시작했습니다. ''';
   static final aboutPara2 =
-  '''작년부터 관심을 가졌던 크로스플랫폼 프레임워크 Flutter를 독학하여 팀 내 프로젝트로 추진했습니다. CI/CD 파이프라인 구축 및 프로토타입 개발을 통해 프로젝트의 가이드를 잡았습니다.''';
+  '''팀 내에선 와이어링 하네스를 설계하는 ECAD를 개발하고 있으며, 신규 기능 개발 및 유지보수가 주 업무입니다.''';
   static final aboutPara3 =
-  '''케이씨 그룹의 유튜브를 보며 사내분위기와 업무환경을 엿볼 수 있었습니다. 앞서 언급한 저의 적극성을 통해 케이씨 그룹에 어울리는 인재가 되겠습니다.''';
+  '''작년부터 관심을 가졌던 크로스플랫폼 프레임워크 Flutter를 독학하여 팀 내 프로젝트로 추진했습니다. CI/CD 파이프라인 구축 및 프로토타입 개발을 통해 프로젝트의 가이드를 잡았습니다.''';
 
   static final techIntro =
   '''기술 스택 :''';
   static final tech1 = ''' .Net'''; // space for manual padding
-  static final tech2 = ''' Flutter'''; // space for manual padding
+  static final tech2 = ''' C#'''; // space for manual padding
   static final tech3 = ''' AWS'''; // space for manual padding
-  static final tech4 = ''' CI/CD'''; // space for manual padding
+  static final tech4 = ''' MongoDB'''; // space for manual padding
 
   /* Exp screen contents */
   static final expCompName1 = '''(주)유라''';
